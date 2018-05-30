643af4088246771fefa26403520dce7c3095380e
valikneskazhu94 <valikneskazhu94@gmail.com>
2018-05-30 21:04:52 +0300

    Pattern_Pizza_Store

---
+++ PizzaStore/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace CoffeeShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Italian Pizza\t",
+            "Bulgarian Pizza"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(338, 66);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Cheese";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(460, 66);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(73, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Pineapple";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(579, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(55, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Olives";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(711, 66);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Mushrooms";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(338, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(23, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(460, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(21, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(579, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(23, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(711, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(22, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(277, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "К-тво:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(402, 431);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PizzaStore/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShop
+{
+    public partial class Form1 : Form
+    {
+        public abstract class Pizza
+        {
+
+            public string description = "Unknown Pizza";
+
+            public abstract double cost();
+            public virtual string getDescription()
+            {
+                return description;
+            }
+        }
+        public class BulgarianPizza : Pizza
+        {
+            public BulgarianPizza()
+            {
+                description = "Bulgarian Pizza";
+            }
+            public override double cost()
+            {
+                return 5.99;
+            }
+        }
+        public class ItalianPizza : Pizza
+        {
+            public ItalianPizza ()
+            {
+                description = "Italian Pizza";
+            }
+            public override double cost ()
+            {
+                return 6.19;
+            }
+             
+
+        }
+        
+        public abstract class ConditionDecorator : Pizza
+        {
+
+            protected Pizza pizza = null;
+            public ConditionDecorator(Pizza pizza)
+            {
+                this.pizza = pizza;
+            }
+
+        }
+        public class Cheese : ConditionDecorator
+        {
+            public Cheese(Pizza pizza) : base(pizza)
+            {
+            }
+
+            public override double cost()
+            {
+                return .31 + pizza.cost();
+            }
+
+            public override string getDescription()
+            {
+                return pizza.getDescription() ;
+            }
+        }
+        public class Pineapple : ConditionDecorator
+        {
+            public Pineapple(Pizza pizza) : base(pizza) { }
+
+            public override double cost()
+            {
+                return .20 + pizza.cost();
+            }
+
+            public override string getDescription()
+            {
+                return pizza.getDescription();
+            }
+        }
+        public class Olives : ConditionDecorator
+        {
+            public Olives(Pizza pizza) : base(pizza) { }
+
+            public override double cost()
+            {
+                return .10 + pizza.cost();
+            }
+
+            public override string getDescription()
+            {
+                return pizza.getDescription() ;
+            }
+        }
+        public class Mushrooms : ConditionDecorator
+        {
+            public Mushrooms(Pizza pizza) : base(pizza) { }
+
+            public override double cost()
+            {
+                return .7 + pizza.cost();
+            }
+
+            public override string getDescription()
+            {
+                return pizza.getDescription();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           Pizza italian  = new ItalianPizza();
+            Pizza bulgarian = new BulgarianPizza();
+           
+            //Mocha mocha = new Mocha(bevarage);
+            //Milk milk = new Milk();
+            if (comboBox1.SelectedItem.ToString() ==italian.getDescription())
+            {
+                checkBox1.Enabled = true; 
+                checkBox2.Enabled = true; 
+                checkBox3.Enabled = true; 
+                checkBox4.Enabled = true;
+                label2.Text = italian.getDescription() + ": " + italian.cost();
+            }
+          if (comboBox1.SelectedItem.ToString() == bulgarian.getDescription())
+            {
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                checkBox3.Enabled = true;
+                checkBox4.Enabled = true;
+                label2.Text=bulgarian.getDescription() + ": " + bulgarian.cost();
+                
+            }
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
